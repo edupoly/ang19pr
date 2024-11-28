51638bb39272e85b2db0e8d0fbbea9f9a9fdea54
edupoly <info.edupoly@gmail.com>
2024-11-28 19:17:12 +0530

    complete level1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@
 import { Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
-  imports:[HomeComponent,RouterOutlet],
+  imports:[HomeComponent,RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
